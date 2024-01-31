0e13bdc62983d10c7d64601ed2d29b9d751e496d
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-01-31 22:43:55 +0000

    cleared player city and added initial city ui

--- overworld/player_city.gd
+++ overworld/player_city.gd
@@ -5,6 +5,7 @@ class_name PlayerCity
 signal switch_worlds_signal
 
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var ui: CanvasLayer = $Ui
 
 func _ready() -> void:
     pass
@@ -14,9 +15,11 @@ func _process(_delta: float) -> void:
 
 func enable():
     self.camera_2d.enabled = true
+    self.ui.visible = true
 
 func disable():
     self.camera_2d.enabled = false
+    self.ui.visible = false
 
 func on_teleport_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
     if event.is_pressed():
