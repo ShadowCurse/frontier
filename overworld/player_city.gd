ead8e90ff989f0b6e535593cf0873de1a1c58364
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-02-01 20:25:48 +0000

    added basic gold mine

--- overworld/player_city.gd
+++ overworld/player_city.gd
@@ -4,13 +4,19 @@ class_name PlayerCity
 
 signal switch_worlds_signal
 signal population_update_signal(int)
+signal gold_update_signal(int)
 
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var ui: CanvasLayer = $Ui
 
 @export var house_scene: PackedScene
+@export var gold_mine_scene: PackedScene
 
 var houses: Array[House] = []
+var gold_mines: Array[GoldMine] = []
+
+var total_population: int = 0
+var total_gold: int = 0
 
 var under_cursor_object: Node2D = null
 
@@ -37,19 +43,28 @@ func on_teleport_input_event(_viewport: Node, event: InputEvent, _shape_idx: int
     if event.is_pressed():
         switch_worlds_signal.emit()
 
-
 func on_player_city_ui_build_house_signal() -> void:
     var house: House = self.house_scene.instantiate()
     under_cursor_object = house
-    house.population_size_increase_signal.connect()
+    house.population_update_signal.connect(on_population_incease_signal)
 
     self.call_deferred("add_child", house)
     self.houses.append(house)
+    
+func on_player_city_ui_build_gold_mine_signal() -> void:
+    var gold_mine: GoldMine = self.gold_mine_scene.instantiate()
+    under_cursor_object = gold_mine
+    gold_mine.gold_update_signal.connect(on_gold_update_signal)
 
-func on_house_size_incease_signal() -> void:
-    var total_population = 0
-    for house in self.houses:
-        total_population += house.current_population
-    self.population_size_increase_signal.emit(total_population)
+    self.call_deferred("add_child", gold_mine)
+    self.gold_mines.append(gold_mine)
+
+func on_population_incease_signal(population: int) -> void:
+    self.total_population += population
+    self.population_update_signal.emit(self.total_population)
+
+func on_gold_update_signal(gold: int) -> void:
+    self.total_gold += gold
+    self.gold_update_signal.emit(self.total_gold)
 
 
--- overworld/player_city_ui.gd
+++ overworld/player_city_ui.gd
@@ -1,8 +1,10 @@
 extends Control
 
 signal build_house_signal
+signal build_gold_mine_signal
 
 @onready var population_label: Label = $resources_panel/MarginContainer/HBoxContainer/population_label
+@onready var gold_label: Label = $resources_panel/MarginContainer/HBoxContainer/gold_label
 
 func _ready() -> void:
     pass
@@ -12,6 +14,12 @@ func _process(_delta: float) -> void:
 
 func on_house_button_pressed() -> void:
     self.build_house_signal.emit()
+    
+func on_gold_mine_button_pressed() -> void:
+    self.build_gold_mine_signal.emit()
 
 func on_population_update_signal(new_population: int) -> void:
     self.population_label.text = "%d" % new_population
+    
+func on_gold_update_signal(new_gold: int) -> void:
+    self.gold_label.text = "%d" % new_gold
