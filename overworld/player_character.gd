e5e74abdc22ba1bcd2679513daab9b15fa3223c8
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-02-02 21:22:30 +0000

    added attack animations for the player

--- overworld/player_character.gd
+++ overworld/player_character.gd
@@ -9,9 +9,13 @@ const SPEED = 300.0
 
 var enabled: bool = true
 var last_facing_direction_left: bool = true
+var is_attacking: bool = false
 
 func _physics_process(_delta: float) -> void:
     if !self.enabled:
+        return
+
+    if Input.is_action_pressed("game_attack"):
         return
 
     var direction := Input.get_vector("game_left", "game_right", "game_up", "game_down")
@@ -24,6 +28,12 @@ func _physics_process(_delta: float) -> void:
         self.velocity.y = move_toward(self.velocity.y, 0, SPEED)
     self.move_and_slide()
 
+func _process(_delta: float) -> void:
+    if !self.enabled:
+        return
+
+    var direction := Input.get_vector("game_left", "game_right", "game_up", "game_down")
+
     # Handle spite directon flipping
     if direction:
         if direction.x <= 0.0:
@@ -35,11 +45,37 @@ func _physics_process(_delta: float) -> void:
     else:
         self.animated_sprite_2d.flip_h = self.last_facing_direction_left
 
+    self.is_attacking = Input.is_action_pressed("game_attack")
+
     # Handle animation
-    if direction:
-        self.animated_sprite_2d.play("run")
+    if self.is_attacking:
+        var mouse_position = get_global_mouse_position()
+        var to_mouse = mouse_position - self.position
+        var angle = to_mouse.angle()
+        # right
+        if -PI / 4.0 <= angle && angle <= PI / 4.0:
+            self.animated_sprite_2d.play("attack_right")
+            self.animated_sprite_2d.flip_h = false
+            self.last_facing_direction_left = false
+        # left
+        if 3.0 * PI / 4.0 <= angle || angle <= -3.0 * PI / 4.0:
+            self.animated_sprite_2d.play("attack_right")
+            self.animated_sprite_2d.flip_h = true
+            self.last_facing_direction_left = true
+        # up
+        if -3.0 * PI / 4.0 <= angle && angle <= -PI / 4.0:
+            self.animated_sprite_2d.play("attack_up")
+        # down
+        if PI / 4.0 <= angle && angle <= 3.0 * PI / 4.0:
+            self.animated_sprite_2d.play("attack_down")
     else:
-        self.animated_sprite_2d.play("idle")
+        if direction:
+            self.animated_sprite_2d.play("run")
+        else:
+            self.animated_sprite_2d.play("idle")
+
+
+
 
 func enable():
     self.camera_2d.enabled = true
