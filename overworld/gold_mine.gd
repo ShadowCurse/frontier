eccc2c834df90bb98fffb7e99fd0bb0df35fc35f
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-02-02 23:53:31 +0000

    added progress bars for resources

--- overworld/gold_mine.gd
+++ overworld/gold_mine.gd
@@ -5,6 +5,7 @@ class_name GoldMine
 signal gold_update_signal(int)
 
 @onready var timer: Timer = $Timer
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 @export var production_speed: float = 0.3
 
@@ -12,8 +13,11 @@ func _ready() -> void:
     pass
 
 func _process(_delta: float) -> void:
+    var timer_time = 1.0 / self.production_speed
+    var progress = (timer_time - self.timer.time_left) / timer_time
+    self.progress_bar.value = progress
     if self.timer.is_stopped():
-        self.timer.start(1.0 / self.production_speed)
+        self.timer.start(timer_time)
 
 func on_timer_timeout() -> void:
     self.gold_update_signal.emit(1)
--- overworld/house.gd
+++ overworld/house.gd
@@ -5,6 +5,7 @@ class_name House
 signal population_update_signal(int)
 
 @onready var timer: Timer = $Timer
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 @export var production_speed: float = 1.0
 @export var max_population: int = 5
@@ -15,8 +16,11 @@ func _ready() -> void:
     pass
 
 func _process(_delta: float) -> void:
+    var timer_time = 1.0 / self.production_speed
+    var progress = (timer_time - self.timer.time_left) / timer_time
+    self.progress_bar.value = progress
     if self.timer.is_stopped() and self.current_population < self.max_population:
-        self.timer.start(1.0 / self.production_speed)
+        self.timer.start(timer_time)
 
 func on_timer_timeout() -> void:
     self.current_population += 1
