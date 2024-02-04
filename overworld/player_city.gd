122168880f3b276a2ab76ec178f3402d258f5587
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-02-04 14:56:33 +0000

    added grid attached placement

--- overworld/player_city.gd
+++ overworld/player_city.gd
@@ -40,15 +40,17 @@ var total_food: int = 0
 var total_wood: int = 0
 
 var under_cursor_object: Node2D = null
+var under_cursor_object_can_place: bool = false
 
 func _ready() -> void:
     var half_offset = self.tile_offset / 2.0
-    var empty_delta = (self.grid_size - self.grid_clear_center_size) / 2
+    var empty_delta = (self.grid_size - self.grid_clear_center_size) / 2.0
     var bottom_left_corner = self.position - Vector2(half_offset * (self.grid_size - 1), half_offset * (self.grid_size - 1))
     for x in range(self.grid_size):
         for y in range(self.grid_size):
             # skip cental nodes
-            if empty_delta <= x && x < (self.grid_size - empty_delta) && empty_delta <= y && y < (self.grid_size - empty_delta):
+            if empty_delta <= x && x < (self.grid_size - empty_delta) \
+              && empty_delta <= y && y < (self.grid_size - empty_delta):
                     continue
             var tile = self.tile_scene.instantiate()
             tile.position = bottom_left_corner + Vector2(x * self.tile_offset, y * self.tile_offset)
@@ -61,10 +63,10 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
     if under_cursor_object:
         var cursor_pos = get_global_mouse_position()
-        under_cursor_object.position = cursor_pos
+        self.move_under_cursor_object(cursor_pos)
 
         if Input.is_action_just_pressed("game_place_object"):
-            under_cursor_object = null
+            self.try_place_object()
 
 func enable():
     self.camera_2d.enabled = true
@@ -73,6 +75,25 @@ func enable():
 func disable():
     self.camera_2d.enabled = false
     self.ui.visible = false
+
+func move_under_cursor_object(cursor_pos: Vector2):
+    for grid_tile_position in self.grid:
+        var tile_left = grid_tile_position.x - self.tile_offset / 2.0
+        var tile_right = grid_tile_position.x + self.tile_offset / 2.0
+        var tile_top = grid_tile_position.y + self.tile_offset / 2.0
+        var tile_bottom = grid_tile_position.y - self.tile_offset / 2.0
+        if tile_left < cursor_pos.x && cursor_pos.x < tile_right \
+          && tile_bottom < cursor_pos.y && cursor_pos.y < tile_top:
+            self.under_cursor_object.position = grid_tile_position
+            self.under_cursor_object_can_place = true
+            return
+
+    self.under_cursor_object_can_place = false
+    self.under_cursor_object.position = cursor_pos
+
+func try_place_object():
+    if self.under_cursor_object_can_place:
+        self.under_cursor_object = null
 
 func on_teleport_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
     if event.is_pressed():
