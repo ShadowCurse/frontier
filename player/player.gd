069abd71df3efe90e74110741ceacf42fc15b2ba
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-02-06 22:02:44 +0000

    added player attack area + states

--- player/player.gd
+++ player/player.gd
@@ -4,89 +4,125 @@ class_name Player
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var weapon_area: Area2D = $weapon_area
 @onready var ui_root: CanvasLayer = $ui_root
 @onready var ui: PlayerUi = $ui_root/ui
 
 @export var speed: float = 600.0
 
-var enabled: bool = true
+enum State {
+    Disabled,
+    Idle,
+    Run,
+    Attack,   
+}
+var current_state: State = State.Disabled
+
+enum AttackDirection {
+    None,
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+var attacking_direction: AttackDirection = AttackDirection.None
 var last_facing_direction_left: bool = true
-var is_attacking: bool = false
 
 func _physics_process(_delta: float) -> void:
-    if !self.enabled:
-        return
-
-    if Input.is_action_pressed("game_attack"):
-        return
-
-    var direction := Input.get_vector("game_left", "game_right", "game_up", "game_down")
-
-    # Handle movement
-    if direction:
-        self.velocity = direction * self.speed
-    else:
-        self.velocity.x = move_toward(self.velocity.x, 0, self.speed)
-        self.velocity.y = move_toward(self.velocity.y, 0, self.speed)
-    self.move_and_slide()
+    match self.current_state:
+        State.Disabled:
+            return
+        State.Idle:
+            return
+        State.Run:
+            var direction := Input.get_vector("game_left", "game_right", "game_up", "game_down")
+            # Handle movement
+            if direction:
+                self.velocity = direction * self.speed
+            else:
+                self.velocity.x = move_toward(self.velocity.x, 0, self.speed)
+                self.velocity.y = move_toward(self.velocity.y, 0, self.speed)
+            self.move_and_slide()
+        State.Attack:
+            pass
 
 func _process(_delta: float) -> void:
-    if !self.enabled:
-        return
-
-    var direction := Input.get_vector("game_left", "game_right", "game_up", "game_down")
-
-    # Handle spite directon flipping
-    if direction:
-        if direction.x <= 0.0:
-            self.animated_sprite_2d.flip_h = true
-            self.last_facing_direction_left = true
-        else:
-            self.animated_sprite_2d.flip_h = false
-            self.last_facing_direction_left = false
-    else:
-        self.animated_sprite_2d.flip_h = self.last_facing_direction_left
-
-    self.is_attacking = Input.is_action_pressed("game_attack")
-
-    # Handle animation
-    if self.is_attacking:
-        var mouse_position = get_global_mouse_position()
-        var to_mouse = mouse_position - self.position
-        var angle = to_mouse.angle()
-        # right
-        if -PI / 4.0 <= angle && angle <= PI / 4.0:
-            self.animated_sprite_2d.play("attack_right")
-            self.animated_sprite_2d.flip_h = false
-            self.last_facing_direction_left = false
-        # left
-        if 3.0 * PI / 4.0 <= angle || angle <= -3.0 * PI / 4.0:
-            self.animated_sprite_2d.play("attack_right")
-            self.animated_sprite_2d.flip_h = true
-            self.last_facing_direction_left = true
-        # up
-        if -3.0 * PI / 4.0 <= angle && angle <= -PI / 4.0:
-            self.animated_sprite_2d.play("attack_up")
-        # down
-        if PI / 4.0 <= angle && angle <= 3.0 * PI / 4.0:
-            self.animated_sprite_2d.play("attack_down")
-    else:
-        if direction:
-            self.animated_sprite_2d.play("run")
-        else:
+    match self.current_state:
+        State.Disabled:
+            return
+        State.Idle:
+            if Input.get_vector("game_left", "game_right", "game_up", "game_down"):
+                self.current_state = State.Run
+                return
+            if Input.is_action_pressed("game_attack"):
+                self.current_state = State.Attack
+                return
             self.animated_sprite_2d.play("idle")
+        State.Run:
+            if not Input.get_vector("game_left", "game_right", "game_up", "game_down"):
+                self.current_state = State.Idle
+                return
+            if Input.is_action_pressed("game_attack"):
+                self.current_state = State.Attack
+                return
+            var direction := Input.get_vector("game_left", "game_right", "game_up", "game_down")
+            # Handle spite directon flipping
+            if direction:
+                if direction.x <= 0.0:
+                    self.animated_sprite_2d.flip_h = true
+                    self.last_facing_direction_left = true
+                else:
+                    self.animated_sprite_2d.flip_h = false
+                    self.last_facing_direction_left = false
+            else:
+                self.animated_sprite_2d.flip_h = self.last_facing_direction_left
+            self.animated_sprite_2d.play("run")
+        State.Attack:
+            if self.attacking_direction != AttackDirection.None:
+                return
+            var mouse_position = get_global_mouse_position()
+            var to_mouse = mouse_position - self.position
+            var angle = to_mouse.angle()
+            # Start motitoring weapon area
+            self.weapon_area.visible = true
+            self.weapon_area.monitoring = true
+            # right
+            if -PI / 4.0 <= angle && angle <= PI / 4.0:
+                self.attacking_direction = AttackDirection.Right
+                self.animated_sprite_2d.play("attack_right")
+                self.animated_sprite_2d.flip_h = false
+                self.last_facing_direction_left = false
+                self.weapon_area.rotation = 0.0
+            # left
+            if 3.0 * PI / 4.0 <= angle || angle <= -3.0 * PI / 4.0:
+                self.attacking_direction = AttackDirection.Left
+                self.animated_sprite_2d.play("attack_right")
+                self.animated_sprite_2d.flip_h = true
+                self.last_facing_direction_left = true
+                self.weapon_area.rotation = PI
+            # up
+            if -3.0 * PI / 4.0 <= angle && angle <= -PI / 4.0:
+                self.attacking_direction = AttackDirection.Up
+                self.animated_sprite_2d.play("attack_up")
+                self.weapon_area.rotation = -PI / 2.0
+            # down
+            if PI / 4.0 <= angle && angle <= 3.0 * PI / 4.0:
+                self.attacking_direction = AttackDirection.Down
+                self.animated_sprite_2d.play("attack_down")
+                self.weapon_area.rotation = PI / 2.0
 
-func enable():
+func enable() -> void:
     self.camera_2d.enabled = true
     self.ui_root.visible = true
     self.visible = true
-    self.enabled = true
+    self.current_state = State.Idle
 
-func disable():
+func disable() -> void:
     self.camera_2d.enabled = false
     self.ui_root.visible = false
     self.visible = false
-    self.enabled = false
+    self.current_state = State.Disabled
 
 func on_yello_village_player_enter_signal() -> void:
     self.ui.show_notification("Found Yello Village")
@@ -96,3 +132,17 @@ func on_purple_village_player_enter_signal() -> void:
 
 func on_red_village_player_enter_signal() -> void:
     self.ui.show_notification("Found Red Village")
+
+func on_animated_sprite_2d_animation_finished() -> void:
+     match self.current_state:
+        State.Disabled:
+            return
+        State.Idle:
+            return
+        State.Run:
+            self.current_state = State.Idle
+        State.Attack:
+            self.current_state = State.Idle
+            self.attacking_direction = AttackDirection.None
+            self.weapon_area.visible = false
+            self.weapon_area.monitoring = false
